ecb5440df4808b5a1952670366d88d56d4f79bf6
Maher Guerfali <maher-guerfali>
2024-03-09 21:00:12 +0100

    pfllmmffezeml

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to="/">
-      <div className=" text-3xl hover:text-red-800 cursor-pointer text-center transition">
+      <div className="font-monoton text-3xl hover:text-red-800 cursor-pointer text-center transition">
         El Karrita
       </div>
     </Link>
